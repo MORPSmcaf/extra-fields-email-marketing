ce71e9959149682572a1bf3ccf69b0040207b8f4
Alex <morps@mcaf.nb.ca>
2023-11-27 11:39:16 -0400

    [ADD] create initial repository

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': "Resource Reservation",
+    'summary': "Resource management platform for managing resources",
+    'version': '16.0.1.0.0',
+    'website': "https://mcaf.nb.ca/en/",
+    'author': "MCAF",
+    'category': "Appointments",
+    'license': 'OPL-1',
+    "application": True,
+    "installable": True,
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/resource_reservation_groups.xml',
+        'security/resource_reservation_security.xml',
+        'views/resource_reservation_views.xml',
+        'views/resource_reservation_tag_views.xml',
+        'views/resource_views.xml',
+        'views/resource_availability.xml',
+        'views/menu.xml'
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ models/resource.py
@@ -0,0 +1,75 @@
+"""Its just show what moduls are imported"""
+from odoo import models, fields, api
+
+
+class Resource(models.Model):
+    """ This module create data for Resource fields in odoo
+        model resource_reservation"""
+    _name = 'resource'
+    _description = 'Resource'
+
+    name = fields.Char(string='Resource name', required=True)
+    resource_type = fields.Many2one(
+        'resource.type',
+        string="Resource Type ", required=True)
+    resource_capacity = fields.Float(string=" Resource Capacity ",
+                                     required=True)
+    resource_capacity_text = fields.Char(string=" Resource Capacity  ",
+                                         required=True)
+    resource_owner = fields.Char(string='Resource owner', required=True)
+    image = fields.Binary(string='')
+    reservation_ids = fields.One2many('resource.reservation',
+                                      'name',
+                                      string='Reservations')
+    confirmed_reservations = fields.One2many('resource.reservation',
+                                             'name',
+                                             compute='_compute_confirmed')
+    cancelled_reservations = fields.One2many('resource.reservation',
+                                             'name',
+                                             compute='_compute_cancelled')
+    resource_tags = fields.Many2many('resource.tag',
+                                     string='Resource Tags ',
+                                     help="Select one or more resource tags",
+                                     widget='many2many_tags')
+
+    @api.depends('reservation_ids')
+    def _compute_confirmed(self):
+        for record in self:
+            record.confirmed_reservations = record.reservation_ids.filtered(
+                lambda r: r.booking_status == 'confirmed')
+
+    @api.depends('reservation_ids')
+    def _compute_cancelled(self):
+        for record in self:
+            record.cancelled_reservations = record.reservation_ids.filtered(
+                lambda r: r.booking_status == 'cancelled')
+
+    @api.onchange('resource_capacity')
+    def _onchange_resource_capacity(self):
+        for record in self:
+            record.resource_capacity_text = str(record.resource_capacity)
+
+
+class ResourceType(models.Model):
+    _name = 'resource.type'
+    _description = 'Resource Type'
+
+    name = fields.Char(string='Resource Type', required=True)
+    color_resource_type = fields.Integer(string="Color")
+
+    _sql_constraints = [
+        ('unique_resource_type', 'UNIQUE (name)',
+         'A resource type with the same name already exists.'),
+    ]
+
+
+class ResourceTag(models.Model):
+    _name = 'resource.tag'
+    _description = 'Resource Tag'
+
+    name = fields.Char(string='Resource Tag', required=True)
+
+    _sql_constraints = [
+        ('unique_resource_tag', 'UNIQUE (name)',
+         'A resource type with the same name already exists.'),
+    ]
